356072861b1a25df8342967c07d64c6e857eee5d
pacificus93 <49984827+pacificus93@users.noreply.github.com>
2023-04-22 15:16:30 -0400

    Create Consecutive-Strings.py

---
+++ Python/6 kyu/Consecutive-Strings.py
@@ -0,0 +1,48 @@
+# -*- coding: utf-8 -*-
+"""
+DESCRIPTION:
+You are given an array(list) strarr of strings and an integer k. Your task is to return the first longest string consisting of k consecutive strings taken in the array.
+
+Examples:
+strarr = ["tree", "foling", "trashy", "blue", "abcdef", "uvwxyz"], k = 2
+
+Concatenate the consecutive strings of strarr by 2, we get:
+
+treefoling   (length 10)  concatenation of strarr[0] and strarr[1]
+folingtrashy ("      12)  concatenation of strarr[1] and strarr[2]
+trashyblue   ("      10)  concatenation of strarr[2] and strarr[3]
+blueabcdef   ("      10)  concatenation of strarr[3] and strarr[4]
+abcdefuvwxyz ("      12)  concatenation of strarr[4] and strarr[5]
+
+Two strings are the longest: "folingtrashy" and "abcdefuvwxyz".
+The first that came is "folingtrashy" so 
+longest_consec(strarr, 2) should return "folingtrashy".
+
+In the same way:
+longest_consec(["zone", "abigail", "theta", "form", "libe", "zas", "theta", "abigail"], 2) --> "abigailtheta"
+n being the length of the string array, if n = 0 or k > n or k <= 0 return "" (return Nothing in Elm, "nothing" in Erlang).
+
+Note
+consecutive strings : follow one after another without an interruption
+"""
+
+def longest_consec(strarr, k):
+    # your code
+    
+    if k <= 0 or k > len(strarr):
+        return ''
+    
+    def concatenate(lst,output = ""):
+        """Concatenating string members of the list"""
+        
+        for i in lst:
+            output += i
+        
+        return output
+    
+    # Concatenating the consecutive strings of strarr by k
+    
+    concatenated_strings = [concatenate(strarr[i:i + k]) for i in range(len(strarr) - k + 1)]
+    length_of_strings = [len(string) for string in concatenated_strings]
+    
+    return [concatenated_strings[i] for i in range(len(concatenated_strings)) if length_of_strings[i] == max(length_of_strings)][0]
